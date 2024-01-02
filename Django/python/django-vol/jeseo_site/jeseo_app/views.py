b4d7b36b13ded542beee098bdefc2593bf03f4eb
jeongyunnim <jeongyun7521@gmail.com>
2024-01-02 21:37:24 +0900

    [Django] for loop 태그 적용

--- Django/python/django-vol/jeseo_site/jeseo_app/views.py
+++
@@ -1,5 +0,0 @@
-from django.shortcuts import render
-
-def example_view(request):
-    # 여기에 있는 jeseo_app은 현재 앱 디렉토리를 가리키지 않는다. jeseo_app/templates/jeseo_app/example.html
-    return render(request, 'jeseo_app/example.html') 

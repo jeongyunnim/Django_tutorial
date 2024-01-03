394ebc2844289dd9f48a3d2b7e7a062005a7038f
jeongyunnim <jeongyun7521@gmail.com>
2024-01-03 15:08:17 +0900

    [Django] 템플릿 상속 학습

--- Django/Django-Lecture/django-vol/jeseo_site/jeseo_app/views.py
+++ Django/Django-Lecture/django-vol/jeseo_site/jeseo_app/views.py
@@ -1,14 +1,7 @@
 from django.shortcuts import render
 
 def example_view(request):
-    # 여기에 있는 jeseo_app은 현재 앱 디렉토리를 가리키지 않는다. jeseo_app/templates/jeseo_app/example.html
     return render(request, 'jeseo_app/example.html') 
 
 def variable_view(request):
-    my_var = {
-        'first_name':'Jeong yun',
-        'last_name':'Seo',
-        'some_list':[1,2,3],
-        'user_logged_in':False
-    }
-    return render(request,'jeseo_app/variable.html', context=my_var)
+    return render(request,'jeseo_app/variable.html')

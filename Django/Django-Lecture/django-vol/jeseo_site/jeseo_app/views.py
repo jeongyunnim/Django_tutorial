d1b3777c33abbe832bfcabb76725710268b31e4d
jeongyunnim <jeongyun7521@gmail.com>
2024-01-04 21:25:18 +0900

    [Feat] Migration 부분까지 완료

--- Django/Django-Lecture/django-vol/jeseo_site/jeseo_app/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-def example_view(request):
-    return render(request, 'jeseo_app/example.html') 
-
-def variable_view(request):
-    return render(request,'jeseo_app/variable.html')
--- Django/Django-Lecture/django-vol/jeseo_site/office/apps.py
+++ Django/Django-Lecture/django-vol/jeseo_site/office/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class JeseoAppConfig(AppConfig):
+class OfficeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'jeseo_app'
+    name = 'office'

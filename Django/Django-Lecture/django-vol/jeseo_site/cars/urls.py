2f04fad3932bc16024f0dac29674d037654c5766
jeongyunnim <jeongyun7521@gmail.com>
2024-01-05 21:39:37 +0900

    [Django] cars 페이지 만들며 복습

---
+++ Django/Django-Lecture/django-vol/jeseo_site/cars/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+# template에서 url 태그를 사용하기 위해 app 이름을 지정
+app_name = 'cars'
+
+urlpatterns = [
+    path('list/', views.list, name='list'),
+    path('add/', views.add, name='add'),
+    path('delete/', views.delete, name='delete'),
+]
